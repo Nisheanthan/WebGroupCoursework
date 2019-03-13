869b1cc09bc69de18b32539a9ec7a64760c5eeda
ChiranRanathunga <chiran.ranathunga@gmail.com>
2019-03-14 03:03:35 +0530

    only add css, scrollUp btn and imgs

--- Student 1/main2.js
+++ Student 1/main2.js
@@ -5,3 +5,21 @@ function resizeText(multiplier) {
   }
   document.body.style.fontSize = parseFloat(document.body.style.fontSize) + (multiplier * 1.0) + "em";
 }
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
+    document.getElementById("scrollUpBtn").style.display = "block";
+  } else {
+    document.getElementById("scrollUpBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
