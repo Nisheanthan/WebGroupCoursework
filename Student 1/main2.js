ff23932182ba91d6dedca0127436a289f6d0ab98
ChiranRanathunga <45303071+ChiranRanathunga@users.noreply.github.com>
2019-03-14 09:42:39 +0530

    html,css,js to qty

--- Student 1/main2.js
+++ Student 1/main2.js
@@ -22,4 +22,21 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
+// Quantity Up Down 
+function increaseValue() {
+  var value = parseInt(document.getElementById('number').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value++;
+  document.getElementById('number').value = value;
+}
 
+function decreaseValue() {
+  var value = parseInt(document.getElementById('number').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value < 1 ? value = 1 : '';
+  value--;
+  document.getElementById('number').value = value;
+}
+function getNumber(){
+  var value = parseInt(document.getElementById('number').value, 10);
+}
